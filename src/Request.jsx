e74e31e2901a7023eb0723f0c066857e061c9d1b
Full Stack Development <fullstackdev@labs.sce.carleton.ca>
2024-05-02 15:16:51 -0400

    hello

---
+++ src/Request.jsx
@@ -0,0 +1,13 @@
+function Request(props) {
+    
+    return (
+        <div>
+
+            <button className= "buttontype2" onClick={props.movies}>Show movies (please note that it may take a while for the movies to show)</button>
+
+        </div>
+    
+    )
+}
+
+export default Request;
